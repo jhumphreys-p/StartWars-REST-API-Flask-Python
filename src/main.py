0ee0a86035a9a7ea19d8d34489571f481060f9ea
jhumphreys-p <jhumphreysp@hotmail.com>
2021-03-22 18:07:50 +0000

    StarWars API

--- src/main.py
+++ src/main.py
@@ -8,7 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Personajes, Planetas, Favoritos
+import datetime
 #from models import Person
 
 app = Flask(__name__)
@@ -33,11 +34,134 @@ def sitemap():
 @app.route('/user', methods=['GET'])
 def handle_hello():
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    query = User.query.all()
+    results = list(map(lambda x: x.serialize(), query))
 
-    return jsonify(response_body), 200
+    return jsonify(results), 200
+
+@app.route('/user/<int:id>', methods=['GET'])
+def macheo_informacion_user(id):
+
+    usuario = User.query.get(id)
+    if usuario is None:
+        raise APIException('Usuario not found', status_code=404)
+    results = usuario.serialize()
+    
+
+    return jsonify(results), 200
+
+@app.route('/personajes', methods=['GET'])
+def personajes_todos():
+
+    query = Personajes.query.all()
+    results = list(map(lambda x: x.serialize2(), query))
+
+    return jsonify(results), 200
+
+@app.route('/personajes/<int:id>', methods=['GET'])
+def personaje_unico(id):
+
+    personaje = Personajes.query.get(id)
+    if personaje is None:
+        raise APIException('Personaje not found', status_code=404)
+    results = personaje.serialize2()
+
+    return jsonify(results), 200
+
+@app.route('/personajes', methods=['POST'])
+def add_personaje():
+
+    # recibir info del request
+    request_body = request.get_json()
+    print(request_body)
+
+    per = Personajes(name=request_body["name"], heigth=request_body["heigth"], mass=request_body["mass"], hair_color=request_body["hair_color"], skin_color=request_body["skin_color"], eye_color=request_body["eye_color"], birth_year=request_body["birth_year"],  gender=request_body["gender"], homeworld=request_body["homeworld"], films=request_body["films"])
+    db.session.add(per)
+    db.session.commit()
+
+    return jsonify("Un exito, se ha agregado el personaje"), 200
+
+
+@app.route('/planetas', methods=['GET'])
+def planetas_todos():
+
+    query = Planetas.query.all()
+    results = list(map(lambda x: x.serialize2(), query))
+
+    return jsonify(results), 200
+
+@app.route('/planetas/<int:id>', methods=['GET'])
+def planeta_unico(id):
+
+    planeta = Planetas.query.get(id)
+    if planeta is None:
+        raise APIException('Planeta not found', status_code=404)
+    results = planeta.serialize2()
+
+    return jsonify(results), 200
+
+@app.route('/planetas', methods=['POST'])
+def add_fav_planeta():
+
+    # recibir info del request
+    request_body = request.get_json()
+    print(request_body)
+
+    per = Planetas(name=request_body["name"], climate=request_body["climate"], orbital_period=request_body["orbital_period"], rotation=request_body["rotation"], terrain=request_body["terrain"])
+    db.session.add(per)
+    db.session.commit()
+
+    return jsonify("Un exito, se ha agregado el planeta"), 200
+
+
+@app.route('/favoritos', methods=['GET'])
+def favoritos_todos():
+
+    query = Favoritos.query.all()
+    results = list(map(lambda x: x.serialize2(), query))
+
+    return jsonify(results), 200
+
+@app.route('/favoritos/<int:id>', methods=['GET'])
+def favoritos_unico(id):
+
+    favoritos = Favoritos.query.get(id)
+    if favoritos is None:
+        raise APIException('Favorito not found', status_code=404)
+    results = favoritos.serialize2()
+
+    return jsonify(results), 200
+
+@app.route('/favoritos', methods=['POST'])
+def add_fav_favoritos():
+
+    # recibir info del request
+    request_body = request.get_json()
+    print(request_body)
+
+    pera = Favoritos(usuario_id=request_body["usuario_id"], planeta_id=request_body["planeta_id"], personaje_id=request_body["personaje_id"])
+    db.session.add(pera)
+    db.session.commit()
+
+    return jsonify("Un exito, se ha agregado el favorito"), 200
+
+
+@app.route('/del_favoritos/<int:id>', methods=['DELETE'])
+def del_fav(id):
+
+    # recibir info del request
+    
+    fav = Favoritos.query.get(id)
+    if fav is None:
+        raise APIException('Favorite not found', status_code=404)
+
+    db.session.delete(fav)
+
+    db.session.commit()
+
+    return jsonify("Borraste bien la informacion"), 200
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -6,14 +6,97 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+    genero = db.Column(db.String(250), nullable=False)
+    
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.name
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "name": self.name,
+            "genero": self.genero
             # do not serialize the password, its a security breach
         }
+
+class Personajes(db.Model):
+    __tablename__ = 'personajes'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    heigth = db.Column(db.String(250), nullable=False)
+    mass = db.Column(db.String(250), nullable=False)
+    hair_color = db.Column(db.String(250), nullable=False)
+    skin_color = db.Column(db.String(250), nullable=False)
+    eye_color = db.Column(db.String(250), nullable=False)
+    birth_year = db.Column(db.String(250), nullable=False)
+    gender = db.Column(db.String(250), nullable=False)
+    homeworld = db.Column(db.String(250), nullable=False)
+    films = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return '<Personajes %r>' % self.name
+
+    def serialize2(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "heigth": self.heigth,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "homeworld": self.homeworld,
+            "films": self.films
+        }
+
+class Planetas(db.Model):
+    __tablename__ = 'planetas'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    climate = db.Column(db.String(250), nullable=False)
+    orbital_period = db.Column(db.String(250), nullable=False)
+    rotation = db.Column(db.String(250), nullable=False)
+    terrain = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return '<Planetas %r>' % self.name
+
+    def serialize2(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "climate": self.climate,
+            "orbital_period": self.orbital_period,
+            "rotation": self.rotation,
+            "terrain": self.terrain
+        }
+
+class Favoritos(db.Model):
+    __tablename__ = 'favoritos'
+    id = db.Column(db.Integer, primary_key=True)
+    usuario_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    planeta_id = db.Column(db.Integer, db.ForeignKey('planetas.id'))
+    personaje_id = db.Column(db.Integer, db.ForeignKey('personajes.id'))
+    personaje = db.relationship("Personajes")
+    planeta = db.relationship("Planetas")
+    usuario = db.relationship("User")
+
+    def __repr__(self):
+        return '<Favoritos %r>' % self.name
+
+
+    def serialize2(self):
+        return {
+            "id": self.id,
+            "usuario_id": self.usuario_id,
+            "planeta_id": self.planeta_id,
+            "personaje_id": self.planeta_id,
+            #"personaje": self.personaje,
+            #"planeta": self.planeta,
+            #"usuario": self.usuario
+        }   
